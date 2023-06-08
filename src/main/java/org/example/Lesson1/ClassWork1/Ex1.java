9b9ef54761ba7a7c1fde26ce2f95280145421681
a-zim <a-zimakov@mail.ru>
2023-06-08 11:43:55 +0300

    hw

--- src/main/java/org/example/Lesson1/ClassWork1/Ex1.java
+++ src/main/java/org/example/Lesson1/ClassWork1/Ex1.java
@@ -1,7 +1,22 @@
 package org.example.Lesson1.ClassWork1;
 
+import java.util.Scanner;
+
 public class Ex1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("zadanie1(n) = " + zadanie1(n));
+    }
 
+    private static int zadanie1(int n) {
+        int multi = 1, sum = 0;
+        while (n != 0) {
+            int m = n % 10;
+            multi *= m;
+            sum += m;
+            n /= 10;
+        }
+        return multi - sum;
     }
 }
--- src/main/java/org/example/Lesson1/HomeWork1/Hw1.java
+++ src/main/java/org/example/Lesson1/HomeWork1/Hw1.java
@@ -1,7 +1,8 @@
 package org.example.Lesson1.HomeWork1;
 
+import java.util.Scanner;
+
 public class Hw1 {
     public static void main(String[] args) {
-
     }
 }
